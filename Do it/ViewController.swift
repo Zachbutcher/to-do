b57f1438eb1d6c953a064ef8ce7c6b8c1ad61aec
Zach Butcher <duke.fan04@yahoo.com>
2017-07-20 06:12:07 -0500

    added task class, segue to add new task to list, populated initial list

--- Do it/ViewController.swift
+++ Do it/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var tableview: UITableView!
+    
+    var tasks :[Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tasks = makeTasks()
+        
+        tableview.dataSource = self
+        tableview.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        
+        
+        if task.important{
+            cell.textLabel?.text = " ‼️ \(task.name)"
+        }else {
+            
+            cell.textLabel?.text = task.name
+        }
+        return cell
+    }
+    
+    @IBAction func addNewTask(_ sender: Any) {
+        performSegue(withIdentifier: "addNewText", sender: nil)
+    }
+    func makeTasks () -> [Task]{
+        let Task1 = Task()
+        Task1.name = "Do Gabby"
+        Task1.important = true
+        let Task2 = Task()
+        Task2.name = "Feed Rhodey"
+        let task3 = Task()
+        task3.name = "Move to Phoenix"
+        
+        return [Task1, Task2, task3]
+    }
+    
 }
 
