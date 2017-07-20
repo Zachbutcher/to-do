2beac67f1d8116b0fee584780ac9bedf45bee033
Zach Butcher <duke.fan04@yahoo.com>
2017-07-20 05:18:06 -0500

    Initial Commit

---
+++ Do it/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Do it
+//
+//  Created by Zach Butcher on 7/20/17.
+//  Copyright © 2017 Zach Butcher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
